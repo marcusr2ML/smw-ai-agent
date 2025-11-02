6126ebca0142071585c2126a51e6ff3d1e5c4746
Marcus Rosales <marcusrayrosales@gmail.com>
2025-11-02 09:22:15 -0600

    Update human_data_collect.py

--- sm_bot/human_data_collect.py
+++ sm_bot/human_data_collect.py
@@ -1,4 +1,3 @@
-import torch
 import numpy as np
 import os
 
@@ -7,30 +6,30 @@ import pickle
 from collections import defaultdict
 
 class HumanDataCollect:
+    #saves actions
     def __init__(self,
                  check_points,
                  y_check_point,
-                 num_episodes,
-                 batch_size=32, 
-                 sync_network_rate=10000):
+                 num_episodes):
         
         
-        self.num_episodes = num_episodes
-        self.check_point_counter = 0
-        self.episode_counter = 0
+        self.num_episodes = num_episodes                 # sets the amount of playthroughs/training data 
         self.check_points = check_points
-        self.y_check_point = y_check_point
-        # Hyperparameters
-        self.batch_size = batch_size
+
+        self.check_point_counter = 0                     # used to keep track of actions that are saved under
+        self.episode_counter = 0                         # a given checkpoint and playthrough
         
-        self.actions_segments = defaultdict(lambda: defaultdict(list))
+        self.y_check_point = y_check_point               # checkpoints above only give x-pos. This is an option  
+                                                         # to require mario to pass through a given y-pos too
+                                                                
+        self.actions_segments = defaultdict(lambda: defaultdict(list))  # how actions are organized for replay to train
 
                 
 
     def save_action(self, action, info, done, truncated):
         
-        if(done or truncated):
-            self.episode_counter += 1
+        if(done or truncated):              #truncate is an extra condition of env.step(), which I did not use in this loop 
+            self.episode_counter += 1       #when done with trial we are on next episode and first checkpoint
             self.check_point_counter = 0
             return None
         
@@ -38,10 +37,35 @@ class HumanDataCollect:
         y = info['y_pos']
         #print(x)
         
-        self.actions_segments[self.check_point_counter ][self.episode_counter].append(action)
+        self.actions_segments[self.check_point_counter ][self.episode_counter].append(action)  #save action segments for future sampeling
         
-        # if(self.check_point_counter<=len(self.check_points)-1 and x>= self.check_points[self.check_point_counter]):
-        #     self.check_point_counter +=1
+     
+        x_check = self.check_points[self.check_point_counter]    #current checkpoint
+        if(x>=x_check):
+            if(x_check == x_check and y == self.y_check_point):  #if we pass the current checkpoint, update checkpoint
+                self.check_point_counter +=1
+                print('check point reached: ', x_check)
+            else:
+                print('Turn around to set the check point!!' ,\
+                      x,',',y, ' --- ' ,self.check_points[self.check_point_counter],\
+                     ',', self.y_check_point )
+        
+        
+        return None
+
+    def store_actions(self):
+        filename="actions_segments_"+str(len(self.check_points))+"_TRIALS_"+str(self.num_episodes)+".pkl"
+        with open(filename, "wb") as f:
+            pickle.dump(dict(self.actions_segments), f)
+            
+            
+    def load_actions(self):
+        filename="actions_segments_"+str(len(self.check_points))+"_TRIALS_"+str(self.num_episodes)+".pkl"
+        with open(filename, "rb") as f:
+            loaded = pickle.load(f)
+            self.actions_segments = defaultdict(lambda: defaultdict(list), loaded)
+            
+        
         #     print('check point reached: ', x)
                 
         
